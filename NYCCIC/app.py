9ae148809fe43e9313d318795251b1c92f61bfca
Deren <de2356@columbia.edu>
2021-04-09 18:30:09 -0400

    fixed indentation, removed altviewer, fixed repeated function names, added HTMLResponse to trend_data_map func, used io and save to return HTML altair plot

--- NYCCIC/app.py
+++ NYCCIC/app.py
@@ -4,7 +4,7 @@
 Main web app
 """
 
-import altair_viewer
+from io import StringIO
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from NYCCIC.map import Map
@@ -17,31 +17,40 @@ app = FastAPI()
 # create a root endpoint that provide basic information about the webapp
 @app.get("/")
 def root():
-  return {"message": """Go to /map or /trend"""} 
+    return {"message": """Go to /map or /trend"""} 
 
 # create endpoint for displaying the map 
 @app.get("/map")
-def root():
-  return {"message": """Go to /PERCPOS or /TESTRT or /DEATHRT or /HOSPRT or /CASERT"""} 
+def map():
+    return {"message": """Go to /PERCPOS or /TESTRT or /DEATHRT or /HOSPRT or /CASERT"""} 
 
 # create endpoint for displaying the trend graph
 @app.get("/trend")
-def root():
-   return {"message": """Go to /TESTRT or /DEATHRT or /HOSPRT or /CASERT"""} 
+def trend():
+    return {"message": """Go to /TESTRT or /DEATHRT or /HOSPRT or /CASERT"""} 
+
 
 # create endpoint for different data type 
 @app.get("/map/{data_type}", response_class=HTMLResponse)
-async def map(data_type: str):
+async def map_data(data_type: str):
     map_class = Map(data_type)
     map_display = map_class.map()
     return map_display._repr_html_()
 
+
 @app.get("/trend/{data_type}")
-def root():
+def trend_data():
     return {"message": """Type desired zipcode- Example:/11201,10001,10002,10005"""}
 
-@app.get("/trend/{data_type}/{zipcode}")
-async def map(data_type: str, zipcode: str):
+
+@app.get("/trend/{data_type}/{zipcode}", response_class=HTMLResponse)
+async def trend_data_zip(data_type: str, zipcode: str):
     graph = Trend(data_type, zipcode)
-    display = graph.trendmap()
-    return altair_viewer.display(display)
+
+    # get the altair chart
+    chart = graph.trendmap()
+
+    # write to HTML string and return
+    htmlchart = StringIO()
+    chart.save(htmlchart, format='html')
+    return htmlchart.getvalue()
