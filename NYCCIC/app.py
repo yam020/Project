bc3019a88a4cde4462a065dc979ebe405fe8d2a5
Yanzhe Ma <ym2827@columbia.edu>
2021-04-20 18:25:20 -0400

    update

--- NYCCIC/app.py
+++ NYCCIC/app.py
@@ -18,7 +18,7 @@ from NYCCIC.trend import Trend
 app = FastAPI()
 
 # create a root endpoint that provide basic information about the webapp
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def root():
     """
     create a root endpoint that provide information about the webapp
@@ -29,15 +29,25 @@ def root():
 
     Returns
     -------
-    A string
+    HTML
     """
-    return {"message": """ Welcome To NYCCIC!
-                            If you want to see the map, go to /map 
-                            If you want to see the trend graphs, go to /trend
-                            Please note they are case sensitive"""}
+    return """
+    <html>
+        <head>
+            <title>Welcome to NYCCIC!</title>
+        </head>
+        <body>
+            <h1> Welcome to NYCCIC! </h1>
+            <p style="text-align: center;"> This website aims to provie the visulization of COVID-19 data in NYC by Zipcode. 
+            <br />If you want to see the map, go to /map.
+            <br />If you want to see the trend graphs, go to /trend.
+            <br /> <em />Please note these are case sensitive. </p>
+        </body>
+    </html>
+    """
 
 # create endpoint for displaying the map
-@app.get("/map")
+@app.get("/map", response_class=HTMLResponse)
 def map():
     """
     create a endpoint for map that provide instructions
@@ -48,18 +58,28 @@ def map():
 
     Returns
     -------
-    A string
+    HTML
     """
-    return {"message": """There are several map you can see
-                         If you want to see the percentage postive map, got to /PERCPOS
-                         If you want to see the death rate map, go to /DEATHRT
-                         If you want to see the hospitalized rate map, go to /HOSPRT
-                         If you want to see the test rate map, go to /TESTRT
-                         If you want to see the case rate map, go to /CASERT
-                         Please note they are case sensitive"""}
+    return """
+    <html>
+        <head>
+            <title>Map Options</title>
+        </head>
+        <body>
+            <h1> Map options </h1>
+            <p> There are several types of map you can see. 
+            <br />If you want to see the percentage postive map, got to /PERCPOS. 
+            <br />If you want to see the death rate map, go to /DEATHRT. 
+            <br />If you want to see the hospitalized rate map, go to /HOSPRT. 
+            <br />If you want to see the test rate map, go to /TESTRT. 
+            <br />If you want to see the case rate map, go to /CASERT. 
+            <br /> <em />Please note those are case sensitive. </p>
+        </body>
+    </html>
+    """
 
 # create endpoint for displaying the trend graph
-@app.get("/trend")
+@app.get("/trend", response_class=HTMLResponse)
 def trend():
     """
     create a endpoint for trend that provide instructions
@@ -70,15 +90,24 @@ def trend():
 
     Returns
     -------
-    A string
+    A html
     """
-    return {"message":  """There are several trend graphs you can see.
-                         If you want to see the death rate trend graph, go to /DEATHRT
-                         If you want to see the hospitalized rate trend graph, go to /HOSPRT
-                         If you want to see the test rate trend graph, go to /TESTRT
-                         If you want to see the case rate trend graph, go to /CASERT
-                         Please note they are case sensitive"""}
-
+    return """
+    <html>
+        <head>
+            <title>Trend Graph Options</title>
+        </head>
+        <body>
+            <h1> Trend Graph options </h1>
+            <p> There are several types of trend graphs you can see. 
+            <br />If you want to see the death rate trend graphs, go to /DEATHRT. 
+            <br />If you want to see the hospitalized rate trend graphs, go to /HOSPRT. 
+            <br />If you want to see the test rate trend graphs, go to /TESTRT. 
+            <br />If you want to see the case rate trend graohs, go to /CASERT. 
+            <br /> <em />Please note those are case sensitive. </p>
+        </body>
+    </html>
+    """
 
 # create endpoint for different data type
 @app.get("/map/{data_type}", response_class=HTMLResponse)
@@ -102,7 +131,7 @@ async def map_data(data_type: str):
     return map_display._repr_html_()
 
 # create endpoint for different data type
-@app.get("/trend/{data_type}")
+@app.get("/trend/{data_type}", response_class=HTMLResponse)
 def trend_data():
     """
     create a endpoint for trend that provide instructions
@@ -113,11 +142,22 @@ def trend_data():
 
     Returns
     -------
-    A string
+    A html
     """
-    return {"message": """Please specify the zipcodes you would like to see
-                          For example,/11201,10001,10002,10005
-                          Please note there should be no space between zipcodes"""}
+    return """
+    <html>
+        <head>
+            <title>Enter the Zipcodes</title>
+        </head>
+        <body>
+            <h1> Enter the zipcodes </h1>
+            <p> Please specify the zipcodes you would like to see. 
+            <br /> For example,
+            <br /> /11201,10001,10002,10005.
+            <br /> <em />Please note there should be no space between zipcodes</p>
+        </body>
+    </html>
+    """
 
 # create endpoint for different data type based on the zipcode users enter
 @app.get("/trend/{data_type}/{zipcode}", response_class=HTMLResponse)
